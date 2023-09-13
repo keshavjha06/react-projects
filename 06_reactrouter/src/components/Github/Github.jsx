0886964bdde75a4018c34151f889db55bcba64f7
Keshav Jha <keshavjha0610@gmail.com>
2023-09-13 22:22:30 +0530

    modified Github.jsx

--- 06_reactrouter/src/components/Github/Github.jsx
+++ 06_reactrouter/src/components/Github/Github.jsx
@@ -3,15 +3,15 @@ import { useLoaderData } from 'react-router-dom'
 
 function Github() {
     const data = useLoaderData()
-    // const [data, setData] = useState([])
-    // useEffect(() => {
-    //  fetch('https://api.github.com/users/keshavjha06')
-    //  .then(response => response.json())
-    //  .then(data => {
-    //     console.log(data);
-    //     setData(data)
-    //  })
-    // }, [])
+    /* const [data, setData] = useState([])
+    useEffect(() => {
+     fetch('https://api.github.com/users/keshavjha06')
+     .then(response => response.json())
+     .then(data => {
+        console.log(data);
+        setData(data)
+     })
+    }, []) */
 
     return (
         <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
