803b081f841fc2e725d0469b52d11759671b1c69
Keshav Jha <keshavjha0610@gmail.com>
2023-09-13 22:06:30 +0530

    Added github.jsx

--- 06_reactrouter/src/main.jsx
+++ 06_reactrouter/src/main.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
+import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 
 /* const router = createBrowserRouter([
   {
     path: '/',
-    element: <Layout />,
+    element: <Layout/>,
     children: [
       {
         path: "",
@@ -36,7 +37,12 @@ const router = createBrowserRouter(
       <Route path='' element={<Home />} />
       <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />} />
-      <Route path='user/:id' element={<User />} />
+      <Route path='user/:userid' element={<User />} />
+      <Route
+        loader={githubInfoLoader}
+        path='github'
+        element={<Github />}
+      />
     </Route>
   )
 )
