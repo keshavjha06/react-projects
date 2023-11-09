e3e5d74db13ef3ce364b08f6720e9dc1177d82e6
Keshav Jha <keshavjha0610@gmail.com>
2023-11-09 16:01:14 +0530

    modified auth.js

--- 11_megablog/src/appwrite/auth.js
+++ 11_megablog/src/appwrite/auth.js
@@ -11,25 +11,25 @@ export class AuthService {
         this.client
             .setEndpoint(conf.appwriteUrl)
             .setProject(conf.appwriteProjectId)
-            this.account = new Account(this.client)
+        this.account = new Account(this.client)
     }
-    async createAccount({email, password , name}) {
+    async createAccount({ email, password, name }) {
         try {
-          const userAccount =   await this.account.create(ID.unique(), email, password, name)
-          if (userAccount) {
-            // call another method
-            return this.login({email, password});
-          } else {
-            return userAccount;
-          }
+            const userAccount = await this.account.create(ID.unique(), email, password, name)
+            if (userAccount) {
+                // call another method
+                return this.login({ email, password });
+            } else {
+                return userAccount;
+            }
         } catch (error) {
             throw error;
         }
     }
 
-    async login({email , password}) {
+    async login({ email, password }) {
         try {
-          return  await this.account.createEmailSession(email , password);
+            return await this.account.createEmailSession(email, password);
         } catch (error) {
             throw error;
         }
@@ -39,17 +39,17 @@ export class AuthService {
         try {
             return await this.account.get();
         } catch (error) {
-            console.log("Appwrite service :: getCurrentUser :: error" , error);
+            console.log("Appwrite service :: getCurrentUser :: error", error);
         }
         return null;
-        
+
     }
 
     async logout() {
         try {
             await this.account.deleteSessions();
         } catch (error) {
-            console.log("Appwrite service :: getCurrentUser :: error", error);     
+            console.log("Appwrite service :: logout :: error", error);
         }
     }
 }
