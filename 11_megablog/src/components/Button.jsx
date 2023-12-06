18aaee6504f69b74f78d9bda7083d51dedcecaf4
Keshav Jha <keshavjha0610@gmail.com>
2023-12-06 19:29:54 +0530

    modified files

--- 11_megablog/src/components/Button.jsx
+++ 11_megablog/src/components/Button.jsx
@@ -1,5 +1,6 @@
+import React from "react";
 
-function Button({
+export default function Button({
     children,
     type = 'button',
     bgColor = 'bg-blue-600',
@@ -11,5 +12,3 @@ function Button({
         <button className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className} `} {...props}>{children}</button>
     )
 }
-
-export default Button
--- 11_megablog/src/components/Logo.jsx
+++ 11_megablog/src/components/Logo.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 
 function Logo({width = '100px'}) {
   return (
